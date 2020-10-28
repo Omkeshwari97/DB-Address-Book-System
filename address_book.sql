5a470d2c7f0bdd077cd08c75219b1923e59da051
Omkeshwari Pilare <omkeshwari1997@gmail.com>
2020-10-29 01:46:22 +0530

    Added funtionality to create address book table

--- address_book.sql
+++ address_book.sql
@@ -4,3 +4,16 @@ show databases;
 create database addressbook_service;
 show databases;
 use addressbook_service;
+
+#uc2
+create table address_book(
+    -> fname varchar(100) not null,
+    -> lname varchar(100) not null,
+    -> address varchar(200) not null,
+    -> city varchar(50) not null,
+    -> zip numeric(6) not null,
+    -> phone numeric(10) not null,
+    -> email nvarchar(255) not null,
+    -> primary key(fname, lname)
+    -> );
+desc address_book;
